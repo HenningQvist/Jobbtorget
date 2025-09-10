93e5f18d36e2f9fcaecee4b1de96d14d800b5f66
HenningQvist <henning.qvist@hotmail.com>
2025-09-10 10:57:12 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -5,9 +5,23 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || undefined,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
-});
+let pool;
+
+if (process.env.NODE_ENV === "production" && process.env.DATABASE_URL) {
+  // Produktion (Railway)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  // Lokal utveckling
+  pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+  });
+}
 
 export default pool;
